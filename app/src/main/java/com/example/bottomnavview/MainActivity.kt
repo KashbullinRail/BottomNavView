1cd87f083fbee2f55774f470c3a3493078ccf998
KashbullinRail <kashbullinrail@gmail.com>
2022-11-01 14:27:52 +0300

    создан слушатель нажатий

--- app/src/main/java/com/example/bottomnavview/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavview/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.bottomnavview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.bottomnavview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.BNV.selectedItemId = R.id.add
+        binding.BNV.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.add -> {Toast.makeText(this, "Add", Toast.LENGTH_LONG).show()}
+                R.id.copy -> {Toast.makeText(this, "Copy", Toast.LENGTH_LONG).show()}
+                R.id.search -> {Toast.makeText(this, "Search", Toast.LENGTH_LONG).show()}
+            }
+        }
+
     }
 }
